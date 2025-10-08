13ed5529f8207f2f3f458e7676dd4c253a844cfe
TkymHrt <23.h.takayama.nutfes@gmail.com>
2025-10-09 03:48:20 +0900

    [fix] meta情報を編集

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -9,8 +9,8 @@ const defaultUrl = process.env.VERCEL_URL
 
 export const metadata: Metadata = {
   metadataBase: new URL(defaultUrl),
-  title: "Next.js and Supabase Starter Kit",
-  description: "The fastest way to build apps with Next.js and Supabase",
+  title: "Half＆Half",
+  description: "総務局を支援する物品管理アプリです。",
 };
 
 const geistSans = Geist({
@@ -25,7 +25,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" suppressHydrationWarning>
+    <html lang="ja" suppressHydrationWarning>
       <body className={`${geistSans.className} antialiased`}>
         <ThemeProvider
           attribute="class"
